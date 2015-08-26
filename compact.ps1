e7502856452b24bf156f499fe934c074cc8f1c5b
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-26 15:04:15 +0200

    Add compact.ps1

---
+++ compact.ps1
@@ -0,0 +1,31 @@
+function Unzip($zip, $destination) {
+    $shell = New-Object -com Shell.Application
+    $zip = $shell.NameSpace($zip)
+    $destination = $shell.Namespace($destination)
+    foreach($item in $zip.items()) {
+        $target = $destination.ParseName($item.Path).Path
+        if (!(Test-Path $target)) {
+            $destination.CopyHere($item)
+        }
+    }
+}
+
+$scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
+
+$architecture = "amd64"
+if ([IntPtr]::size -eq 4) {
+    $architecture = "i386"
+}
+
+Unzip "$($scriptDir)\scripts\ultradefrag-portable-6.1.0.bin.$($architecture).zip" "C:\Windows\Temp"
+Unzip "$($scriptDir)\scripts\SDelete.zip" "C:\Windows\Temp"
+
+net stop wuauserv
+Remove-Item C:\Windows\SoftwareDistribution\Download -Recurse -Force
+mkdir C:\Windows\SoftwareDistribution\Download
+net start wuauserv
+
+& C:\Windows\Temp\ultradefrag-portable-6.1.0.$architecture\udefrag.exe --optimize --repeat C:
+
+reg add HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
+& C:\Windows\Temp\sdelete.exe -q -z C:
