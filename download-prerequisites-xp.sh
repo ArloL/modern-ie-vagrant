e2d5e91d4dfdd75348d2a092e073bc9925f16ba9
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-27 16:38:29 +0200

    Continue downloads by default

--- download-prerequisites-xp.sh
+++ download-prerequisites-xp.sh
@@ -5,14 +5,14 @@ set -x
 
 cd scripts
 
-wget -N http://download.microsoft.com/download/0/8/c/08c19fa4-4c4f-4ffb-9d6c-150906578c9e/NetFx20SP1_x86.exe
+wget -c -N http://download.microsoft.com/download/0/8/c/08c19fa4-4c4f-4ffb-9d6c-150906578c9e/NetFx20SP1_x86.exe
 shasum --check NetFx20SP1_x86.exe.sha1
 
-wget -N http://download.microsoft.com/download/E/C/E/ECE99583-2003-455D-B681-68DB610B44A4/WindowsXP-KB968930-x86-ENG.exe
+wget -c -N http://download.microsoft.com/download/E/C/E/ECE99583-2003-455D-B681-68DB610B44A4/WindowsXP-KB968930-x86-ENG.exe
 shasum --check WindowsXP-KB968930-x86-ENG.exe.sha1
 
-wget -N http://schinagl.priv.at/nt/hardlinkshellext/driver/symlink-1.06-x86.cab
+wget -c -N http://schinagl.priv.at/nt/hardlinkshellext/driver/symlink-1.06-x86.cab
 shasum --check symlink-1.06-x86.cab.sha1
 
-wget -N http://downloads.sourceforge.net/ultradefrag/ultradefrag-portable-6.1.0.bin.i386.zip
+wget -c -N http://downloads.sourceforge.net/ultradefrag/ultradefrag-portable-6.1.0.bin.i386.zip
 shasum --check ultradefrag-portable-6.1.0.bin.i386.zip.sha1
--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -5,8 +5,8 @@ set -x
 
 cd scripts
 
-wget -N http://download.sysinternals.com/files/SDelete.zip
+wget -c -N http://download.sysinternals.com/files/SDelete.zip
 shasum --check SDelete.zip.sha1
 
-wget -N http://downloads.sourceforge.net/ultradefrag/ultradefrag-portable-6.1.0.bin.amd64.zip
+wget -c -N http://downloads.sourceforge.net/ultradefrag/ultradefrag-portable-6.1.0.bin.amd64.zip
 shasum --check ultradefrag-portable-6.1.0.bin.amd64.zip.sha1
