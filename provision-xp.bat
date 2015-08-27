de7602b0a48edaa203d84b951d807d9c222d605d
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-27 15:22:07 +0200

    Check for missing files

--- provision-xp.bat
+++ provision-xp.bat
@@ -1,7 +1,10 @@
 @echo on
 
 %~dp0scripts\NetFx20SP1_x86.exe /q /norestart
+if %ERRORLEVEL% EQU 9009 echo "Prerequisites missing "&& pause && exit /b
+
 %~dp0scripts\WindowsXP-KB968930-x86-ENG.exe /quiet /passive /norestart
+if %ERRORLEVEL% EQU 9009 echo "Prerequisites missing "&& pause && exit /b
 
 call %~dp0scripts\provision-fake-mklink.bat
 
