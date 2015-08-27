8eb774465da9f9e18011782d867f455ba2e75241
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-27 15:22:24 +0200

    Delete scheduled task before scheduling so we don't get conflicts

--- scripts/provision-fake-mklink.bat
+++ scripts/provision-fake-mklink.bat
@@ -4,4 +4,5 @@ expand %~dp0symlink-1.06-x86.cab -f:* C:\Windows\system32
 copy /Y %~dp0mklink.cmd C:\Windows\system32
 copy /Y %~dp0Symlink.ps1 C:\Windows\system32
 
+schtasks /delete /tn "senable install" /f
 schtasks /create /tn "senable install" /tr "senable install" /sc onstart /ru ""
