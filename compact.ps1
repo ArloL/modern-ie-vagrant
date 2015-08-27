2f5dfc90afa91e1c0785b4a6454cce3e3c793283
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-27 14:50:27 +0200

    Allow overriding when unzipping

--- compact.ps1
+++ compact.ps1
@@ -1,8 +1,11 @@
-function Unzip($zip, $destination) {
+function Unzip($zip, $destination, $overwrite = $false) {
     $shell = New-Object -COMObject "Shell.Application"
     $zip = $shell.NameSpace($zip)
     foreach($item in $zip.items()) {
         $target = "$($destination)\$($item.Path)"
+        if ($overwrite) {
+            Remove-Item $target -Recurse -Force
+        }
         if (!(Test-Path $target)) {
             $shell.NameSpace($destination).CopyHere($item)
         }
