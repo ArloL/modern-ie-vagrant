306cd01695313d1d8e667d675860243329e6ad0b
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-28 16:22:34 +0200

    Use only the leaf portion of the item path

--- compact.ps1
+++ compact.ps1
@@ -2,7 +2,7 @@ function Unzip($zip, $destination, $overwrite = $false) {
     $shell = New-Object -COMObject "Shell.Application"
     $zip = $shell.NameSpace($zip)
     foreach($item in $zip.items()) {
-        $target = "$($destination)\$($item.Path)"
+        $target = "$($destination)\$($item.Path | Split-Path -Leaf)"
         if ((Test-Path $target) -And $overwrite) {
             Remove-Item $target -Recurse -Force
         }
