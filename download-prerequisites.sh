05b1dc63c6c379c5fe38790a0e6b64f3d717f711
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 10:38:47 +0100

    change case of sdelete.zip

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -6,7 +6,7 @@ set -o xtrace
 cd scripts
 
 wget --continue --timestamping https://download.sysinternals.com/files/sdelete.zip
-shasum --check SDelete.zip.sha1
+shasum --check sdelete.zip.sha1
 
 wget --continue --timestamping http://downloads.sourceforge.net/ultradefrag/ultradefrag-portable-7.0.0.bin.amd64.zip
 shasum --check ultradefrag-portable-7.0.0.bin.amd64.zip.sha1
