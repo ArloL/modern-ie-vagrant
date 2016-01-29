e32eb49b3aa89503dbd61f8be72ed0f32be542a2
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-01-29 20:26:25 +0100

    Call elevate directly

--- elevate-provision-vista-ie7.bat
+++ elevate-provision-vista-ie7.bat
@@ -1,3 +1,1 @@
-mkdir C:\tmp
-copy \\VBOXSRV\vagrant\elevate.exe C:\tmp
-C:\tmp\elevate.exe -c \\VBOXSRV\vagrant\provision-vista-ie7.bat
+\\VBOXSRV\vagrant\elevate.exe -c \\VBOXSRV\vagrant\provision-vista-ie7.bat
