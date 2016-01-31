b43ce0506957872954e75c6cbcfddb6e46013100
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-01-31 14:58:49 +0100

    Download elevate

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -10,3 +10,5 @@ shasum --check sdelete.zip.sha1
 
 wget --continue --timestamping http://downloads.sourceforge.net/ultradefrag/ultradefrag-portable-7.0.0.bin.amd64.zip
 shasum --check ultradefrag-portable-7.0.0.bin.amd64.zip.sha1
+
+wget --continue --timestamping http://code.kliu.org/misc/elevate/elevate-1.3.0-redist.7z
