23786318752f9bc7157fe1b3a07b7ba64d43299f
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-03 18:05:54 +0100

    Wait in all elevate scripts

--- elevate-provision-vista-ie7.bat
+++ elevate-provision-vista-ie7.bat
@@ -1,1 +1,1 @@
-\\VBOXSRV\vagrant\scripts\elevate-1.3.0-redist\bin.x68-32\elevate.exe -c \\VBOXSRV\vagrant\provision-vista-ie7.bat
+\\VBOXSRV\vagrant\scripts\elevate-1.3.0-redist\bin.x68-32\elevate.exe -c -w \\VBOXSRV\vagrant\provision-vista-ie7.bat
