5a30be56d28d653f3535dcebb83ff6e1cb91749a
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-03 20:59:32 +0100

    Take a snapshot and execute provisioning right at the start to save some time

--- automated-winxp-ie8.sh
+++ automated-winxp-ie8.sh
@@ -27,6 +27,8 @@ wait_for_guestcontrol "${VM}" 2
 
 sleep 10
 
+VBoxManage snapshot "${VM}" take "Snapshot $(date -u +"%Y-%m-%dT%H:%M:%SZ")" --live
+
 # altPress, tabPress, tabRelease, altRelease
 VBoxManage controlvm "${VM}" keyboardputscancode 38 0f 8f b8
 # tabPress, tabRelease
@@ -37,6 +39,8 @@ VBoxManage controlvm "${VM}" keyboardputscancode 1c 9c
 wait_for_guestcontrol "${VM}" 3
 
 sleep 60
+
+VBoxManage guestcontrol "${VM}" --verbose --username IEUser --password 'Passw0rd!' run --exe "//VBOXSRV/vagrant/provision-${box_name}.bat"
 
 # remove wga
 
@@ -70,10 +74,6 @@ sleep 60
 VBoxManage controlvm "${VM}" keyboardputscancode 1c 9c
 
 wait_for_guestcontrol "${VM}" 3
-
-sleep 60
-
-VBoxManage guestcontrol "${VM}" --verbose --username IEUser --password 'Passw0rd!' run --exe "//VBOXSRV/vagrant/provision-${box_name}.bat"
 
 vagrant provision
 
