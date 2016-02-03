90dbe34d45fbb46e0ffbca3cbb952beacf20a833
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-03 19:45:55 +0100

    Add automated-winxp-ie8.sh

---
+++ automated-winxp-ie8.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+wait_for_guestcontrol() {
+    GuestAdditionsRunLevel=0
+    while true ; do
+        echo "Waiting for ${1} to be available for guestcontrol."
+        eval "$(VBoxManage showvminfo "${1}" --machinereadable | grep 'GuestAdditionsRunLevel')"
+        if [ "${GuestAdditionsRunLevel}" -eq "${2}" ]; then
+            return 0;
+        fi
+        sleep 5
+    done
+}
+
+export box_name=winxp-ie8
+export boot_timeout=5
+
+vagrant up || true
+
+VM=$(cat .vagrant/machines/default/virtualbox/id)
+
+wait_for_guestcontrol "${VM}" 2
+
+sleep 10
+
+# altPress, tabPress, tabRelease, altRelease
+VBoxManage controlvm "${VM}" keyboardputscancode 38 0f 8f b8
+# tabPress, tabRelease
+VBoxManage controlvm "${VM}" keyboardputscancode 0f 8f
+# enterPress, enterRelease
+VBoxManage controlvm "${VM}" keyboardputscancode 1c 9c
+
+wait_for_guestcontrol "${VM}" 3
+
+sleep 60
+
+VBoxManage guestcontrol "${VM}" --verbose --username IEUser --password 'Passw0rd!' start --exe "//VBOXSRV/vagrant/scripts/removewga.exe"
+
+sleep 5
+
+# enterPress, enterRelease
+VBoxManage controlvm "${VM}" keyboardputscancode 1c 9c
+
+sleep 1
+
+# enterPress, enterRelease
+VBoxManage controlvm "${VM}" keyboardputscancode 1c 9c
+
+wait_for_guestcontrol "${VM}" 0
+
+wait_for_guestcontrol "${VM}" 2
+
+sleep 60
+
+# enterPress, enterRelease
+VBoxManage controlvm "${VM}" keyboardputscancode 1c 9c
+
+wait_for_guestcontrol "${VM}" 3
+
+sleep 60
+
+VBoxManage guestcontrol "${VM}" --verbose --username IEUser --password 'Passw0rd!' run --exe "//VBOXSRV/vagrant/provision-${box_name}.bat"
+
+vagrant provision
+
+vagrant package --output "${box_name}.box" --Vagrantfile Vagrantfile-package
