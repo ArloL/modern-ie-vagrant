c6cfef7bcc67c4343b02a6728d0f26df6bb09c33
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-03 18:01:34 +0100

    Use the 64bit elevate for windows 10

--- elevate-provision-win10-edge.bat
+++ elevate-provision-win10-edge.bat
@@ -1,1 +1,1 @@
-\\VBOXSRV\vagrant\scripts\elevate-1.3.0-redist\bin.x86-32\elevate.exe  -c \\VBOXSRV\vagrant\provision-win10-edge.bat
+\\VBOXSRV\vagrant\scripts\elevate-1.3.0-redist\bin.x86-64\elevate.exe  -c \\VBOXSRV\vagrant\provision-win10-edge.bat
