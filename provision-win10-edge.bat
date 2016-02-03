450cf93b58c653b7c3bcad4c1811f226ebdf5d2f
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-03 17:55:42 +0100

    Call Set-NetConnectionProfile directly

--- provision-win10-edge.bat
+++ provision-win10-edge.bat
@@ -1,5 +1,5 @@
 @echo on
 
-powershell.exe -ExecutionPolicy Bypass -Command "& {Set-NetConnectionProfile -NetworkCategory Private}"
+powershell.exe -ExecutionPolicy Bypass -Command "Set-NetConnectionProfile -NetworkCategory Private"
 
 call %~dp0scripts\provision-winrm.bat
