b7458b92b0c199b5e0727ca5cb84958ff27fa1e2
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-04 12:09:03 +0100

    Update win7-ie11 script

--- automated-win7-ie11.sh
+++ automated-win7-ie11.sh
@@ -25,34 +25,65 @@ VM=$(cat .vagrant/machines/default/virtualbox/id)
 
 wait_for_guestcontrol "${VM}" 3
 
-sleep 60
+sleep 90
 
-VBoxManage snapshot "${VM}" take "Snapshot $(date -u +"%Y-%m-%dT%H:%M:%SZ")" --live
+VBoxManage snapshot "${VM}" list || VBoxManage snapshot "${VM}" take "Snapshot 0" --live
 
-# select home network
+# close restart dialog
 
-# upPress, upRelease
-VBoxManage controlvm "${VM}" keyboardputscancode 48 c8
-# upPress, upRelease
-VBoxManage controlvm "${VM}" keyboardputscancode 48 c8
+# escPress, escRelease
+VBoxManage controlvm "${VM}" keyboardputscancode 01 81
+
+sleep 5
+
+# Press Win+R so we can open the \\vboxsrv directory and execute our batch
+# script from there.
+
+# leftWindowPress, rPress, rRelease, leftWindowRelease
+VBoxManage controlvm "${VM}" keyboardputscancode e0 5b 13 93 e0 db
+
+sleep 5
+
+#  Enter \\vboxsrv and press ENTER
+
+VBoxManage controlvm "${VM}" keyboardputscancode 2b ab
+VBoxManage controlvm "${VM}" keyboardputscancode 2b ab
+VBoxManage controlvm "${VM}" keyboardputscancode 2f af
+VBoxManage controlvm "${VM}" keyboardputscancode 30 b0
+VBoxManage controlvm "${VM}" keyboardputscancode 18 98
+VBoxManage controlvm "${VM}" keyboardputscancode 2d ad
+VBoxManage controlvm "${VM}" keyboardputscancode 1f 9f
+VBoxManage controlvm "${VM}" keyboardputscancode 13 93
+VBoxManage controlvm "${VM}" keyboardputscancode 2f af
+
 # enterPress, enterRelease
 VBoxManage controlvm "${VM}" keyboardputscancode 1c 9c
 
 sleep 15
 
-# close dialog
+# Make sure the folder is available so we can run our script from there
 
-# escPress, escRelease
-VBoxManage controlvm "${VM}" keyboardputscancode 01 81
+VBoxManage guestcontrol "${VM}" --verbose --username IEUser --password 'Passw0rd!' stat //VBOXSRV/vagrant
 
 { VBoxManage guestcontrol "${VM}" --verbose --username IEUser --password 'Passw0rd!' run --exe "//VBOXSRV/vagrant/elevate-provision-${box_name}.bat"; } &
 
 sleep 15
 
+# altPress
+VBoxManage controlvm "${VM}" keyboardputscancode 38
+# tabPress, tabRelease
+VBoxManage controlvm "${VM}" keyboardputscancode 0f 8f
+# altRelease
+VBoxManage controlvm "${VM}" keyboardputscancode b8
+
+sleep 5
+
 # select Yes on UAC
 
-# leftPress, leftRelease
-VBoxManage controlvm "${VM}" keyboardputscancode 4b cb
+# tabPress, tabRelease
+VBoxManage controlvm "${VM}" keyboardputscancode 0f 8f
+VBoxManage controlvm "${VM}" keyboardputscancode 0f 8f
+VBoxManage controlvm "${VM}" keyboardputscancode 0f 8f
 # enterPress, enterRelease
 VBoxManage controlvm "${VM}" keyboardputscancode 1c 9c
 
