04609fcfa592f9bd034edc21fa58df9ec16ded69
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-02-07 19:42:09 +0100

    Update scripts for win8-ie10

--- elevate-provision-win8-ie10.bat
+++ elevate-provision-win8-ie10.bat
@@ -1,1 +1,1 @@
-\\VBOXSRV\vagrant\scripts\elevate-1.3.0-redist\bin.x86-32\elevate.exe -c -w \\VBOXSRV\vagrant\provision-win8-ie10.bat
+\\VBOXSRV\vagrant\scripts\elevate-1.3.0-redist\bin.x86-64\elevate.exe -c -w \\VBOXSRV\vagrant\provision-win8-ie10.bat
