7c00ed7f9e437a5137bfb8a31f5463b42a95d460
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-09-29 12:42:02 +0200

    Correct case of SDelete.zip and SDelete.zip.sha1

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -5,8 +5,8 @@ set -o xtrace
 
 cd scripts
 
-wget --continue --timestamping https://download.sysinternals.com/files/sdelete.zip
-shasum --check sdelete.zip.sha1
+wget --continue --timestamping https://download.sysinternals.com/files/SDelete.zip
+shasum --check SDelete.zip.sha1
 
 wget --continue --timestamping http://downloads.sourceforge.net/ultradefrag/ultradefrag-portable-7.0.0.bin.amd64.zip
 shasum --check ultradefrag-portable-7.0.0.bin.amd64.zip.sha1
