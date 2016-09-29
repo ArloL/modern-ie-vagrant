acc8514c6501753638cbc8332c96400e2662959f
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-09-29 15:46:59 +0200

    Increase the maximum values

--- provision-win7-ie8.bat
+++ provision-win7-ie8.bat
@@ -5,7 +5,7 @@ powershell.exe -ExecutionPolicy Bypass -File %~dp0scripts\provision-network-priv
 call %~dp0scripts\provision-winrm.bat
 
 set WINRM_EXEC=call %SYSTEMROOT%\system32\winrm
-%WINRM_EXEC% set winrm/config/winrs @{MaxShellsPerUser="1000"}
+%WINRM_EXEC% set winrm/config/winrs @{MaxShellsPerUser="999999999"}
 %WINRM_EXEC% set winrm/config/winrs @{MaxConcurrentUsers="100"}
-%WINRM_EXEC% set winrm/config/winrs @{MaxProcessesPerShell="1000"}
-%WINRM_EXEC% set winrm/config/service @{MaxConcurrentOperationsPerUser="1000"}
+%WINRM_EXEC% set winrm/config/winrs @{MaxProcessesPerShell="999999999"}
+%WINRM_EXEC% set winrm/config/service @{MaxConcurrentOperationsPerUser="999999999"}
