dbca8e9d8c17512932feb171f7730624022bdd63
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-10-18 10:26:27 +0200

    Update download url

--- download-prerequisites-xp.sh
+++ download-prerequisites-xp.sh
@@ -14,7 +14,7 @@ shasum --check WindowsXP-KB968930-x86-ENG.exe.sha1
 wget --continue --timestamping http://schinagl.priv.at/nt/hardlinkshellext/driver/symlink-1.06-x86.cab
 shasum --check symlink-1.06-x86.cab.sha1
 
-wget --continue --timestamping http://downloads.sourceforge.net/ultradefrag/ultradefrag-portable-7.0.0.bin.i386.zip
+wget --continue --timestamping https://sourceforge.net/projects/ultradefrag/files/stable-release/7.0.0/ultradefrag-portable-7.0.0.bin.i386.zip/download
 shasum --check ultradefrag-portable-7.0.0.bin.i386.zip.sha1
 
 wget --continue --timestamping http://files1.majorgeeks.com/4ab35559cc92240547c18e228f234484/system/removewga.exe
