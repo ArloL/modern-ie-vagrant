3835f0d8e0511b071e2f94d9cc4c8daa365e70c8
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-11-30 13:40:06 +0100

    Remove win8 stuff

--- elevate-provision-win8-ie10.bat
+++
@@ -1,1 +0,0 @@
-\\VBOXSRV\vagrant\scripts\elevate-1.3.0-redist\bin.x86-64\elevate.exe -c -w \\VBOXSRV\vagrant\provision-win8-ie10.bat
--- provision-win8-ie10.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-
-powershell.exe -ExecutionPolicy Bypass -File %~dp0scripts\provision-network-private.ps1
-
-call %~dp0scripts\provision-winrm.bat
