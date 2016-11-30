3fe3de9d8584da7686934fe504f886022bf72a26
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-11-30 14:31:45 +0100

    Remove workaround

--- provision-win7-ie8.bat
+++ provision-win7-ie8.bat
@@ -3,11 +3,3 @@
 powershell.exe -ExecutionPolicy Bypass -File %~dp0scripts\provision-network-private.ps1
 
 call %~dp0scripts\provision-winrm.bat
-
-rem Workaround for https://github.com/mitchellh/vagrant/issues/7489
-
-set WINRM_EXEC=call %SYSTEMROOT%\system32\winrm
-%WINRM_EXEC% set winrm/config/winrs @{MaxShellsPerUser="999999999"}
-%WINRM_EXEC% set winrm/config/winrs @{MaxConcurrentUsers="100"}
-%WINRM_EXEC% set winrm/config/winrs @{MaxProcessesPerShell="999999999"}
-%WINRM_EXEC% set winrm/config/service @{MaxConcurrentOperationsPerUser="999999999"}
