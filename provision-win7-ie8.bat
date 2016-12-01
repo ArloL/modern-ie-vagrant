fa24da572948137f65282e3e9eba64d2f0210c87
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-12-01 08:21:06 +0100

    Add oracle to trusted publishers

--- provision-win7-ie8.bat
+++ provision-win7-ie8.bat
@@ -3,3 +3,5 @@
 powershell.exe -ExecutionPolicy Bypass -File %~dp0scripts\provision-network-private.ps1
 
 call %~dp0scripts\provision-winrm.bat
+
+for %%i in (%~dp0scripts\vbox_*.cer) do certutil -addstore -f "TrustedPublisher" %%i
