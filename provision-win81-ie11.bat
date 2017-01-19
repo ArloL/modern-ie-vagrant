b3a4691eead75464c14637bf9bc275388c407328
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-01-19 16:32:02 +0100

    Do not call Set-NetConnectionProfile

--- provision-win81-ie11.bat
+++ provision-win81-ie11.bat
@@ -1,6 +1,6 @@
 @echo on
 
-powershell.exe -ExecutionPolicy Bypass -Command "Set-NetConnectionProfile -NetworkCategory Private"
+powershell.exe -ExecutionPolicy Bypass -File %~dp0scripts\provision-network-private.ps1
 
 call %~dp0scripts\provision-winrm.bat
 
