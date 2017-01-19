32a0a85ed5edf766672225a4bab76e3e913d572f
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-01-19 16:17:53 +0100

    Open \\vboxsrv before trying to provision

--- automated-win81-ie11.sh
+++ automated-win81-ie11.sh
@@ -33,8 +33,34 @@ sleep 60
 
 VBoxManage snapshot "${VM}" list || VBoxManage snapshot "${VM}" take "Snapshot 0" --live
 
+# Press Win+R so we can open the \\vboxsrv directory and execute our batch
+# script from there.
+
+# leftWindowPress, rPress, rRelease, leftWindowRelease
+VBoxManage controlvm "${VM}" keyboardputscancode e0 5b 13 93 e0 db
+
+sleep 15
+
+#  Enter \\vboxsrv and press ENTER
+
+VBoxManage controlvm "${VM}" keyboardputscancode 2b ab
+VBoxManage controlvm "${VM}" keyboardputscancode 2b ab
+VBoxManage controlvm "${VM}" keyboardputscancode 2f af
+VBoxManage controlvm "${VM}" keyboardputscancode 30 b0
+VBoxManage controlvm "${VM}" keyboardputscancode 18 98
+VBoxManage controlvm "${VM}" keyboardputscancode 2d ad
+VBoxManage controlvm "${VM}" keyboardputscancode 1f 9f
+VBoxManage controlvm "${VM}" keyboardputscancode 13 93
+VBoxManage controlvm "${VM}" keyboardputscancode 2f af
+
 # enterPress, enterRelease
 VBoxManage controlvm "${VM}" keyboardputscancode 1c 9c
+
+sleep 30
+
+# Make sure the folder is available so we can run our script from there
+
+VBoxManage guestcontrol "${VM}" --verbose --username IEUser --password 'Passw0rd!' stat //VBOXSRV/vagrant
 
 { VBoxManage guestcontrol "${VM}" --verbose --username IEUser --password 'Passw0rd!' run --exe "//VBOXSRV/vagrant/elevate-provision-${box_name}.bat"; } &
 
