5ddcad5ca03ace7d7616ea3628d4b8ad61a0b1b0
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-01-20 09:08:24 +0100

    Add extraction of VBoxGuestAdditions

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -9,6 +9,8 @@ wget --continue --timestamping http://code.kliu.org/misc/elevate/elevate-1.3.0-r
 
 wget --continue --timestamping http://download.virtualbox.org/virtualbox/5.1.14/VBoxGuestAdditions_5.1.14.iso
 
+7z x VBoxGuestAdditions_5.1.14.iso -y -o$(pwd)/VBoxGuestAdditions
+
 wget --continue --timestamping https://download.sysinternals.com/files/SDelete.zip
 shasum --check SDelete.zip.sha1
 
