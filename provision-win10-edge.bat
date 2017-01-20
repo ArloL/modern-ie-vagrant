8a811c288767d55cebd631d2851e3510df734820
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-01-20 11:56:39 +0100

    Add all provision steps to provision script

--- provision-win10-edge.bat
+++ provision-win10-edge.bat
@@ -1,9 +1,19 @@
 @echo on
 
-powershell.exe -ExecutionPolicy Bypass -Command "Set-NetConnectionProfile -NetworkCategory Private"
+copy %~dp0scripts\provision-network-private.ps1 C:\Users\IEUser\provision-network-private.ps1
+
+copy %~dp0scripts\vagrant-onstart.cmd C:\Users\IEUser\vagrant-onstart.cmd
+
+PowerShell -ExecutionPolicy Bypass -File C:\Users\IEUser\provision-network-private.ps1
 
 net start schedule
 
 call %~dp0scripts\provision-winrm.bat
 
 for %%i in (%~dp0scripts\VBoxGuestAdditions\cert\vbox-*.cer) do certutil -addstore -f "TrustedPublisher" %%i
+
+powershell -File \\VBOXSRV\vagrant\hello.ps1
+
+schtasks /Create /SC ONSTART /TN "vagrant-onstart" /TR "C:\Users\IEUser\vagrant-onstart.cmd" /RL HIGHEST /DELAY 0000:20 /F
+
+\\VBOXSRV\vagrant\scripts\VBoxGuestAdditions\VBoxWindowsAdditions.exe /S
