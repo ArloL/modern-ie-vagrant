946cb0c3259129f80650bf052bc9718732798bf0
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-01-20 09:26:30 +0100

    Install certs from guest additions

--- provision-win10-preview-edge.bat
+++ provision-win10-preview-edge.bat
@@ -6,4 +6,4 @@ net start schedule
 
 call %~dp0scripts\provision-winrm.bat
 
-for %%i in (%~dp0scripts\vbox_*.cer) do certutil -addstore -f "TrustedPublisher" %%i
+for %%i in (%~dp0scripts\VBoxGuestAdditions\cert\vbox-*.cer) do certutil -addstore -f "TrustedPublisher" %%i
--- provision-win7-ie10.bat
+++ provision-win7-ie10.bat
@@ -4,4 +4,4 @@ powershell.exe -ExecutionPolicy Bypass -File %~dp0scripts\provision-network-priv
 
 call %~dp0scripts\provision-winrm.bat
 
-for %%i in (%~dp0scripts\vbox_*.cer) do certutil -addstore -f "TrustedPublisher" %%i
+for %%i in (%~dp0scripts\VBoxGuestAdditions\cert\vbox-*.cer) do certutil -addstore -f "TrustedPublisher" %%i
