867afbc16162f1d4ddee9c6b598a8fa61b3e255d
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-01-23 18:19:10 +0100

    Add automated.sh

---
+++ automated.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+sh automated-win7-ie8.sh
+vagrant destroy --force
+
+sh automated-win7-ie9.sh
+vagrant destroy --force
+
+sh automated-win7-ie10.sh
+vagrant destroy --force
+
+sh automated-win7-ie11.sh
+vagrant destroy --force
+
+sh automated-win81-ie11.sh
+vagrant destroy --force
+
+sh automated-win10-edge.sh
+vagrant destroy --force
+
+sh automated-win10-preview-edge.sh
+vagrant destroy --force
