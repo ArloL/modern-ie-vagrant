c3f20c0cc9609f9c1848b49aaea5370f3901abd5
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-01-27 13:11:01 +0100

    Unzip SDelete and ultradefrag

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -16,5 +16,11 @@ wget --continue --timestamping http://download.virtualbox.org/virtualbox/5.1.14/
 wget --continue --timestamping https://download.sysinternals.com/files/SDelete.zip
 shasum --check SDelete.zip.sha1
 
+unzip -o SDelete.zip -d SDelete
+
 wget --continue --timestamping --output-document ultradefrag-portable-7.0.1.bin.amd64.zip https://sourceforge.net/projects/ultradefrag/files/stable-release/7.0.1/ultradefrag-portable-7.0.1.bin.amd64.zip/download
 shasum --check ultradefrag-portable-7.0.1.bin.amd64.zip.sha1
+
+unzip -o ultradefrag-portable-7.0.1.bin.amd64.zip
+
+mv ultradefrag-portable-7.0.1.amd64 ultradefrag
