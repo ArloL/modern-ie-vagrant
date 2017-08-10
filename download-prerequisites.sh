686130e432db0128036f5bb8f6455deffecea663
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-08-10 14:34:35 +0200

    virtualbox guest additions:5.1.18 -> 5.1.26

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -9,9 +9,9 @@ wget --continue --timestamping http://code.kliu.org/misc/elevate/elevate-1.3.0-r
 
 7z x elevate-1.3.0-redist.7z -y -o$(pwd)/elevate
 
-wget --continue --timestamping http://download.virtualbox.org/virtualbox/5.1.18/VBoxGuestAdditions_5.1.18.iso
+wget --continue --timestamping http://download.virtualbox.org/virtualbox/5.1.26/VBoxGuestAdditions_5.1.26.iso
 
-7z x VBoxGuestAdditions_5.1.18.iso -y -o$(pwd)/VBoxGuestAdditions
+7z x VBoxGuestAdditions_5.1.26.iso -y -o$(pwd)/VBoxGuestAdditions
 
 wget --continue --timestamping https://download.sysinternals.com/files/SDelete.zip
 shasum --check SDelete.zip.sha1
