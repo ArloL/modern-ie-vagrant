e5081c01d8f9891223c5f08af8f95cb9c744948d
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-09-22 17:12:22 +0200

    Also set DefaultUserName and DefaultPassword

--- provision.bat
+++ provision.bat
@@ -1,5 +1,7 @@
 @echo on
 
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d "IEUser" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "Passw0rd!" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d "1" /f
 
 copy %~dp0scripts\provision-network-private.ps1 C:\Users\IEUser\provision-network-private.ps1
