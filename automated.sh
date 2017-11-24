225da1d42964e029c9493bb92b5a744c8e5e45b3
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-11-24 10:01:07 +0100

    Catch erros when trying to destroy non-existing vm

--- automated.sh
+++ automated.sh
@@ -7,22 +7,22 @@ set -o xtrace
 vagrant destroy --force || true
 
 sh automated-win7-ie8.sh
-vagrant destroy --force
+vagrant destroy --force || true
 
 sh automated-win7-ie9.sh
-vagrant destroy --force
+vagrant destroy --force || true
 
 sh automated-win7-ie10.sh
-vagrant destroy --force
+vagrant destroy --force || true
 
 sh automated-win7-ie11.sh
-vagrant destroy --force
+vagrant destroy --force || true
 
 sh automated-win81-ie11.sh
-vagrant destroy --force
+vagrant destroy --force || true
 
 sh automated-win10-edge-no-additions.sh
-vagrant destroy --force
+vagrant destroy --force || true
 
 sh automated-win10-preview-edge.sh
-vagrant destroy --force
+vagrant destroy --force || true
