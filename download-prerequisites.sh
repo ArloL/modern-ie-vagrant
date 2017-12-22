7f8b00ba40743b95bcab669a3c636c8d80ad63cc
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-12-22 15:29:28 +0100

    Use latest vboxguestadditions

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -9,6 +9,6 @@ wget --continue --timestamping http://code.kliu.org/misc/elevate/elevate-1.3.0-r
 
 7z x elevate-1.3.0-redist.7z -y -o$(pwd)/elevate
 
-wget --continue --timestamping http://download.virtualbox.org/virtualbox/5.1.28/VBoxGuestAdditions_5.1.28.iso
+wget --continue --timestamping http://download.virtualbox.org/virtualbox/5.2.4/VBoxGuestAdditions_5.2.4.iso
 
-7z x VBoxGuestAdditions_5.1.28.iso -y -o$(pwd)/VBoxGuestAdditions
+7z x VBoxGuestAdditions_5.2.4.iso -y -o$(pwd)/VBoxGuestAdditions
