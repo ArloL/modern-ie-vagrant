debc3c6e7f135872f93e9f9168dcb66ea88d3d75
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-12-29 13:29:54 +0100

    Latest win10-edge box has guest additions

--- automated.sh
+++ automated.sh
@@ -21,7 +21,7 @@ vagrant destroy --force || true
 sh automated-win81-ie11.sh
 vagrant destroy --force || true
 
-sh automated-win10-edge-no-additions.sh
+sh automated-win10-edge.sh
 vagrant destroy --force || true
 
 # 2017-12-29: Not available any more
