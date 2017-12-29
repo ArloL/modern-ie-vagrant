226b8726174d0983c39622497bdbbf2febc89d9e
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2017-12-29 12:03:07 +0100

    Remove win10-preview-edge from automated.sh

--- automated.sh
+++ automated.sh
@@ -24,5 +24,6 @@ vagrant destroy --force || true
 sh automated-win10-edge-no-additions.sh
 vagrant destroy --force || true
 
-sh automated-win10-preview-edge.sh
-vagrant destroy --force || true
+# 2017-12-29: Not available any more
+# sh automated-win10-preview-edge.sh
+# vagrant destroy --force || true
