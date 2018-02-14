8613b065d9bb740c604635022cae04c7bc029df9
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2018-02-14 13:40:53 +0100

    Create win10-preview-edge again

--- automated.sh
+++ automated.sh
@@ -24,6 +24,5 @@ vagrant destroy --force || true
 sh automated-win10-edge.sh
 vagrant destroy --force || true
 
-# 2017-12-29: Not available any more
-# sh automated-win10-preview-edge.sh
-# vagrant destroy --force || true
+sh automated-win10-preview-edge.sh
+vagrant destroy --force || true
