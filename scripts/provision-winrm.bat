c44ba5a6dd1ecddd324f169b9a2e1ac5ba3be533
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2018-08-30 15:23:32 +0200

    Increase memory for winrm shells

--- scripts/provision-winrm.bat
+++ scripts/provision-winrm.bat
@@ -2,7 +2,7 @@
 
 set WINRM_EXEC=call %SYSTEMROOT%\system32\winrm
 %WINRM_EXEC% quickconfig -q
-%WINRM_EXEC% set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
+%WINRM_EXEC% set winrm/config/winrs @{MaxMemoryPerShellMB="1024"}
 %WINRM_EXEC% set winrm/config @{MaxTimeoutms="1800000"}
 %WINRM_EXEC% set winrm/config/client/auth @{Basic="true"}
 %WINRM_EXEC% set winrm/config/service @{AllowUnencrypted="true"}
