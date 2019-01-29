10181cfe2104854851a5a4e1e3605ffa0e011d8a
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2019-01-29 10:30:07 +0100

    use win10-edge script w/o guest additions

--- automated.sh
+++ automated.sh
@@ -21,7 +21,9 @@ vagrant destroy --force || true
 sh automated-win81-ie11.sh
 vagrant destroy --force || true
 
-sh automated-win10-edge.sh
+# 2018-01-29: Currently not with guest additions
+#sh automated-win10-edge.sh
+sh automated-win10-edge-no-additions.sh
 vagrant destroy --force || true
 
 # 2018-08-30: Currently no download available
