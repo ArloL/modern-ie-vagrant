875d4b1c9e1ca493807065a2121bceab619aaa21
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-03-03 12:04:11 +0100

    Update virtualbox

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -9,6 +9,6 @@ wget --continue --timestamping http://code.kliu.org/misc/elevate/elevate-1.3.0-r
 
 7z x elevate-1.3.0-redist.7z -y -o$(pwd)/elevate
 
-wget --continue --timestamping http://download.virtualbox.org/virtualbox/6.0.4/VBoxGuestAdditions_6.0.4.iso
+wget --continue --timestamping http://download.virtualbox.org/virtualbox/6.1.4/VBoxGuestAdditions_6.1.4.iso
 
-7z x VBoxGuestAdditions_6.0.4.iso -y -o$(pwd)/VBoxGuestAdditions
+7z x VBoxGuestAdditions_6.1.4.iso -y -o$(pwd)/VBoxGuestAdditions
