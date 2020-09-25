1260f4e0514197f76dd5f4ae174497fc5ae570bd
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-09-25 07:54:22 +0200

    Set BackgroundType to solid color

--- in-action/provision-bginfo.ps1
+++ in-action/provision-bginfo.ps1
@@ -4,5 +4,7 @@ reg add "HKCU\Control Panel\Colors" /v Background /t REG_SZ /d "0 99 177" /f
 reg delete "HKCU\Control Panel\Desktop" /v Wallpaper /f
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /f
 
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" /v BackgroundType /t REG_DWORD /d 1 /f
+
 add-type -typedefinition "using System;`n using System.Runtime.InteropServices;`n public class PInvoke { [DllImport(`"user32.dll`")] public static extern bool SetSysColors(int cElements, int[] lpaElements, int[] lpaRgbValues); }"
 [PInvoke]::SetSysColors(1, @(1), @(0xB16300))
