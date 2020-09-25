157c39f51820113d2eaf338631786fb22222ad85
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-09-25 07:36:24 +0200

    Call SetSysColors to immediately change bg color

--- in-action/provision-bginfo.ps1
+++ in-action/provision-bginfo.ps1
@@ -3,3 +3,6 @@ reg add "HKCU\Control Panel\Colors" /v Background /t REG_SZ /d "0 99 177" /f
 
 reg delete "HKCU\Control Panel\Desktop" /v Wallpaper /f
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /f
+
+add-type -typedefinition "using System;`n using System.Runtime.InteropServices;`n public class PInvoke { [DllImport(`"user32.dll`")] public static extern bool SetSysColors(int cElements, int[] lpaElements, int[] lpaRgbValues); }"
+[PInvoke]::SetSysColors(1, @(1), @(0xB16300))
