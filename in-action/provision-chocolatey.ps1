f67252320c30f1431d4ab8b2a30a529e32e7fa95
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-10-03 00:27:28 +0200

    Check powershell version before installing chocolatey

--- in-action/provision-chocolatey.ps1
+++ in-action/provision-chocolatey.ps1
@@ -1,1 +1,5 @@
+if ($PSVersionTable.PSVersion.Major -lt 3) {
+    throw "Wrong PowerShell version"
+}
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
