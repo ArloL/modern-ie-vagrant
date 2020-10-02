05ed14fcde62dcaf748515c522a147b8b3dc5cf4
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-10-03 01:06:33 +0200

    Refactor add-all-boxes.sh

--- add-all-boxes.sh
+++ add-all-boxes.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -o errexit
 set -o nounset
 set -o xtrace
 
-for i in `ls -1 *.box`; do
-    box_name=$(basename $i | cut -d. -f1)
-    vagrant box add --name "okeeffe-${box_name}" --force "${name}.box"
+for i in *.box
+do
+    [[ -e "$i" ]] || break  # handle the case of no *.box files
+    box_name=$(basename "$i" | cut -d. -f1)
+    vagrant box add --name "okeeffe-${box_name}" --force "${box_name}.box"
 done
