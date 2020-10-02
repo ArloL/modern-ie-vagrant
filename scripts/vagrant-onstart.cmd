606181a395ded5d6ff6b2f0b2e2d18cb847cc0b0
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-10-02 21:26:50 +0200

    Enable PSRemoting on startup

--- scripts/vagrant-onstart.cmd
+++ scripts/vagrant-onstart.cmd
@@ -3,3 +3,4 @@
 rmdir "C:\tmp" /S /Q
 
 PowerShell -ExecutionPolicy Bypass -File C:\Users\IEUser\provision-network-private.ps1
+PowerShell -ExecutionPolicy Bypass -File C:\Users\IEUser\provision-psremoting.ps1
