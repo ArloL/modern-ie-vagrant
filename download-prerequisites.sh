fff62ca8c48456faea0da544d5843013c5b91069
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-10-05 18:17:49 +0200

    Make download-prerequisites download quiet

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -5,6 +5,6 @@ set -o xtrace
 
 cd scripts
 
-wget --continue --timestamping http://download.virtualbox.org/virtualbox/6.1.14/VBoxGuestAdditions_6.1.14.iso
+wget --quiet --continue --timestamping http://download.virtualbox.org/virtualbox/6.1.14/VBoxGuestAdditions_6.1.14.iso
 
 7z x VBoxGuestAdditions_6.1.14.iso -y -o"$(pwd)/VBoxGuestAdditions"
