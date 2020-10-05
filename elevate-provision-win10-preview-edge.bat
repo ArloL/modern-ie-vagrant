31430ba0156b1b935eecace10381fe861f85db46
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-10-05 16:10:01 +0200

    Use powershell to elevate provision script

--- elevate-provision-win10-preview-edge.bat
+++ elevate-provision-win10-preview-edge.bat
@@ -1,1 +1,1 @@
-\\VBOXSRV\vagrant\scripts\elevate\bin.x86-64\elevate.exe -c -w \\VBOXSRV\vagrant\provision.bat
+powershell -Command "&{ Start-Process cmd -ArgumentList '/c \\VBOXSRV\vagrant\provision.bat' -Verb RunAs}"
--- elevate-provision-win7-ie9.bat
+++ elevate-provision-win7-ie9.bat
@@ -1,1 +1,1 @@
-\\VBOXSRV\vagrant\scripts\elevate\bin.x86-32\elevate.exe -c -w \\VBOXSRV\vagrant\provision.bat
+powershell -Command "&{ Start-Process cmd -ArgumentList '/c \\VBOXSRV\vagrant\provision.bat' -Verb RunAs}"
