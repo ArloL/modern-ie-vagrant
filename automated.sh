4ff7a6196c3813b782e083fc9e1c210ce90b36fa
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-10-21 23:03:43 +0200

    Do not destroy between scripts

--- automated.sh
+++ automated.sh
@@ -7,19 +7,8 @@ set -o xtrace
 vagrant destroy --force || true
 
 ./automated-win.sh win7-ie8
-vagrant destroy --force || true
-
 ./automated-win.sh win7-ie9
-vagrant destroy --force || true
-
 ./automated-win.sh win7-ie10
-vagrant destroy --force || true
-
 ./automated-win.sh win7-ie11
-vagrant destroy --force || true
-
 ./automated-win.sh win81-ie11
-vagrant destroy --force || true
-
 ./automated-win.sh win10-edge
-vagrant destroy --force || true
