58960bb4d35f0049b0ba67b77455bcaa94d0c06b
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-10-25 07:30:59 +0100

    Update other win10 scripts

--- automated-win10-edge-no-additions.sh
+++ automated-win10-edge-no-additions.sh
@@ -8,13 +8,21 @@ set -o xtrace
 
 box_name=win10-edge
 
+# We want the variable to expand when setting the trap
+# shellcheck disable=SC2064
+trap "vagrant halt ${box_name} --force" EXIT
+
 if [ -f ".vagrant/machines/${box_name}/virtualbox/id" ]; then
     VM=$(cat ".vagrant/machines/${box_name}/virtualbox/id")
 else
     VM=""
 fi
 
-if VBoxManage snapshot "${VM}" list; then
+if [ "${VM}" != "" ] && VBoxManage snapshot "${VM}" list; then
+
+    VBoxManage modifyvm "${VM}" \
+        --recording "on" \
+        --recordingfile "recordings/${box_name}-$(date -u +"%Y%m%dT%H%M%S").webm"
 
     boot_timeout=15 vagrant snapshot restore "${box_name}" "Snapshot 0" || true
 
@@ -24,9 +32,9 @@ else
 
     VM=$(cat ".vagrant/machines/${box_name}/virtualbox/id")
 
-    sleep 60
+    sleep 120
 
-    wait_for_guest_additions_run_level "${VM}" 2
+    wait_for_guest_additions_run_level "${VM}" 2 600
 
     sleep 60
     sleep 60
@@ -44,19 +52,16 @@ VBoxManage storageattach "${VM}" --storagectl "IDE Controller" --port 1 --device
 sleep 60
 
 # Press Win+R
-send_keys "<winPress>" r "<winRelease>"
+send_keys 4 "<winPress>" r "<winRelease>"
 
-sleep 15
+sleep 10
 
-send_keys "e:\\vboxwindowsadditions /S" "<enter>"
+send_keys 14 "e:\\vboxwindowsadditions /S" "<enter>"
 
-sleep 15
+sleep 60
 
 # select Yes on UAC
-
-send_keys "<left>" "<enter>"
-
-sleep 15
+send_keys 14 "<left>" "<enter>"
 
 wait_for_guest_additions_run_level "${VM}" 1
 
@@ -65,13 +70,12 @@ sleep 60
 VBoxManage storageattach "${VM}" --storagectl "IDE Controller" --port 1 --device 1 --type dvddrive --medium emptydrive
 
 # Press Win+R
-send_keys "<winPress>" r "<winRelease>"
+send_keys 4 "<winPress>" r "<winRelease>"
 
-sleep 15
+sleep 10
 
 # Enter shutdown /r /t 0 and press ENTER
-
-send_keys "shutdown /r /t 0" "<enter>"
+send_keys 14 "shutdown /r /t 0" "<enter>"
 
 wait_for_guest_additions_run_level "${VM}" 3
 
@@ -79,35 +83,35 @@ sleep 60
 
 # Press Win+R so we can open the \\vboxsrv directory and execute our batch
 # script from there.
-send_keys "<winPress>" r "<winRelease>"
+send_keys 4 "<winPress>" r "<winRelease>"
 
-sleep 15
+sleep 10
 
-send_keys "\\\\vboxsrv" "<enter>"
+send_keys 14 "\\\\vboxsrv\\vagrant\\scripts\\elevate-provision.bat" "<enter>"
 
-sleep 30
-
-# Make sure the folder is available so we can run our script from there
-
-VBoxManage guestcontrol "${VM}" --verbose --username IEUser --password 'Passw0rd!' stat //VBOXSRV/vagrant
-
-{ VBoxManage guestcontrol "${VM}" --username IEUser --password 'Passw0rd!' run --exe "//VBOXSRV/vagrant/elevate-provision.bat"; } &
-
-provisionPID=$!
-
-sleep 15
+sleep 60
 
 # select Yes on UAC
+send_keys 14 "<left>" "<enter>"
 
-send_keys "<left>" "<enter>"
+sleep 240
 
-wait ${provisionPID} || true
-
-wait_for_vm_to_shutdown "${VM}"
+wait_for_vm_to_shutdown "${VM}" 1200
 
 vagrant up "${box_name}" --provision
 vagrant reload "${box_name}" --provision
 vagrant halt "${box_name}"
+
+VBoxManage modifyvm "${VM}" --recording off
+
+VBoxManage setextradata "${VM}" "GUI/Fullscreen"
+VBoxManage setextradata "${VM}" "GUI/LastCloseAction"
+VBoxManage setextradata "${VM}" "GUI/LastGuestSizeHint"
+VBoxManage setextradata "${VM}" "GUI/LastNormalWindowPosition"
+VBoxManage setextradata "${VM}" "GUI/RestrictedRuntimeDevicesMenuActions"
+VBoxManage setextradata "${VM}" "GUI/RestrictedRuntimeMachineMenuActions"
+VBoxManage setextradata "${VM}" "GUI/ScaleFactor"
+VBoxManage setextradata "${VM}" "GUI/StatusBar/IndicatorOrder"
 
 vagrant package "${box_name}" --output "${box_name}.box" --Vagrantfile Vagrantfile-package
 
