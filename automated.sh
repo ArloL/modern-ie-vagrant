96d44761c48fca73e37792a78198a5c9a70d57fd
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-11-01 16:29:11 +0100

    Update automated.sh

--- automated.sh
+++ automated.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 set -o nounset
-set -o xtrace
 
-./automated-win.sh win7-ie8
-./automated-win.sh win7-ie9
-./automated-win.sh win7-ie10
-./automated-win.sh win7-ie11
-./automated-win.sh win81-ie11
-./automated-win.sh win10-edge
+GUEST_ADDITIONS_INSTALL_MODE="${1:-auto}"
+
+./automated-win.sh win7-ie8 "${GUEST_ADDITIONS_INSTALL_MODE}"
+./automated-win.sh win7-ie9 "${GUEST_ADDITIONS_INSTALL_MODE}"
+./automated-win.sh win7-ie10 "${GUEST_ADDITIONS_INSTALL_MODE}"
+./automated-win.sh win7-ie11 "${GUEST_ADDITIONS_INSTALL_MODE}"
+./automated-win.sh win81-ie11 "${GUEST_ADDITIONS_INSTALL_MODE}"
+./automated-win.sh win10-edge "${GUEST_ADDITIONS_INSTALL_MODE}"
