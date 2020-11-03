c9b969d71418d680d4c3e99873f295a486ab1e5e
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-11-03 08:56:02 +0100

    Execute uninstall only if it exists

--- scripts/unprovision.ps1
+++ scripts/unprovision.ps1
@@ -1,5 +1,8 @@
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d "0" /f
 
-& "C:\Program Files\Oracle\VirtualBox Guest Additions\uninst.exe" /S
+$target="C:\Program Files\Oracle\VirtualBox Guest Additions\uninst.exe"
+if (Test-Path $target) {
+    & $target /S
+}
 
 shutdown /s /t 30 /f
