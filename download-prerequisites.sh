5450e80829b3e3ef222370e1e914b2554ce6d4b6
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-11-04 13:42:19 +0100

    Add PowerShell KB as prerequisite download

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -10,3 +10,7 @@ latest=$(curl -s https://download.virtualbox.org/virtualbox/LATEST-STABLE.TXT)
 wget --quiet --continue --timestamping "https://download.virtualbox.org/virtualbox/${latest}/VBoxGuestAdditions_${latest}.iso"
 
 7z x "VBoxGuestAdditions_${latest}.iso" -y -o"$(pwd)/VBoxGuestAdditions"
+
+wget --quiet --continue --timestamping --output-document=Win7-KB3191566-x86.zip "https://go.microsoft.com/fwlink/?linkid=839522"
+
+7z x "Win7-KB3191566-x86.zip" -y -o"$(pwd)/Win7-KB3191566-x86"
--- scripts/provision-wmf51.ps1
+++ scripts/provision-wmf51.ps1
@@ -3,25 +3,10 @@ if ($PSVersionTable.PSVersion.Major -ge 3) {
     return
 }
 
-if (Get-HotFix -Id KB3191566) {
-    echo "KB3191566 is already installed"
-    return
-}
-
 $scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
 
-. $scriptDir\Unzip.ps1
-
-$path = Join-Path $env:TEMP "Win7-KB3191566-x86"
-
-md $path -Force
-
-(New-Object System.Net.WebClient).DownloadFile("https://go.microsoft.com/fwlink/?linkid=839522", "$path\Win7-KB3191566-x86.zip")
-
-Unzip "$path\Win7-KB3191566-x86.zip" $path
+$path = Join-Path $scriptDir "Win7-KB3191566-x86"
 
 While (!(Get-HotFix -Id KB3191566)) {
     wusa.exe "$path/Win7-KB3191566-x86.msu" /quiet /norestart | Out-Null
 }
-
-Remove-Item $path -Recurse -Force
