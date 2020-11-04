df1de95b502d529e3bd64897975648da0fbfd7b9
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-11-04 13:48:46 +0100

    Add optional parameter to download-prerequisites

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -o errexit
+set -o nounset
 set -o xtrace
 
 cd scripts
@@ -11,6 +12,12 @@ wget --quiet --continue --timestamping "https://download.virtualbox.org/virtualb
 
 7z x "VBoxGuestAdditions_${latest}.iso" -y -o"$(pwd)/VBoxGuestAdditions"
 
-wget --quiet --continue --timestamping --output-document=Win7-KB3191566-x86.zip "https://go.microsoft.com/fwlink/?linkid=839522"
-
-7z x "Win7-KB3191566-x86.zip" -y -o"$(pwd)/Win7-KB3191566-x86"
+case ${1:-} in
+    "win7"*)
+        ;&
+    "")
+        wget --quiet --continue --timestamping --output-document=Win7-KB3191566-x86.zip \
+            "https://go.microsoft.com/fwlink/?linkid=839522"
+        7z x "Win7-KB3191566-x86.zip" -y -o"$(pwd)/Win7-KB3191566-x86"
+        ;;
+esac
