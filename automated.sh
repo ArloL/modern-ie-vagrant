3eab47d51f00f2d21387209d3534c42501c44f1a
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2020-11-06 13:15:12 +0100

    Add reset parameter

--- automated.sh
+++ automated.sh
@@ -3,10 +3,11 @@
 set -o nounset
 
 install_mode="${1:-auto}"
+reset="${2:-}"
 
-./automated-win.sh win7-ie8 "${install_mode}"
-./automated-win.sh win7-ie9 "${install_mode}"
-./automated-win.sh win7-ie10 "${install_mode}"
-./automated-win.sh win7-ie11 "${install_mode}"
-./automated-win.sh win81-ie11 "${install_mode}"
-./automated-win.sh win10-edge "${install_mode}"
+./automated-win.sh win7-ie8 "${install_mode}" "${reset}"
+./automated-win.sh win7-ie9 "${install_mode}" "${reset}"
+./automated-win.sh win7-ie10 "${install_mode}" "${reset}"
+./automated-win.sh win7-ie11 "${install_mode}" "${reset}"
+./automated-win.sh win81-ie11 "${install_mode}" "${reset}"
+./automated-win.sh win10-edge "${install_mode}" "${reset}"
