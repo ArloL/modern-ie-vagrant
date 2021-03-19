5e946a5f35bc3d8709ed60c4769e350a934a70bb
Arlo O'Keeffe <tiger@k5d.de>
2021-03-19 06:34:05 +0100

    add release script

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+MAJOR_MINOR=$(date -u +"%Y.%-m")
+git fetch --prune --prune-tags --tags --force
+
+for MICRO in $(seq 0 999); do
+    VERSION=${MAJOR_MINOR}.${MICRO}
+    if git tag "v${VERSION}"; then
+        break
+    fi
+done
+
+git push origin "${VERSION}"
+echo "::set-output name=version::${VERSION}"
