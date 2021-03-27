625a565eee2d29d3c9abd2f742c52793eca4af59
Arlo O'Keeffe <a.okeeffe@sprinteins.com>
2021-03-28 00:47:32 +0100

    set more powersettings

--- in-action/provision-powersettings.ps1
+++ in-action/provision-powersettings.ps1
@@ -1,1 +1,10 @@
+powercfg -change -standby-timeout-ac 0
+powercfg -change -standby-timeout-dc 0
+
 powercfg -change -monitor-timeout-ac 0
+powercfg -change -monitor-timeout-dc 0
+
+powercfg -change -disk-timeout-ac 0
+powercfg -change -disk-timeout-dc 0
+
+powercfg -h off
