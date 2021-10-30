ffb110ebc92033ce0d2a79de72a9ac27a69b79bb
Arlo O'Keeffe <tiger@k5d.de>
2021-10-30 13:23:17 +0200

    change version schema

--- release.sh
+++ release.sh
@@ -4,10 +4,10 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-MAJOR_MINOR=$(date -u +"%Y.%-m")
+MAJOR_MINOR=$(date -u +"%y%m")
 git fetch --prune --prune-tags --tags --force
 
-for MICRO in $(seq 0 999); do
+for MICRO in $(seq 1 999); do
     VERSION=${MAJOR_MINOR}.${MICRO}
     if git tag "v${VERSION}" && git push origin "v${VERSION}"; then
         break
